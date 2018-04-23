ec1df9935d4615396fd92da835809a8a5178c694
Future <liyongkui@yayuanzi.com>
2018-04-23 23:45:47 +0800

    ADDED:添加常见问题cell

---
+++ pe762-ios/Classes/PersonCenter/View/ProblemTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ProblemTableViewCell.m
+//  pe762-ios
+//
+//  Created by Future on 2018/4/23.
+//  Copyright © 2018年 zmit. All rights reserved.
+//  常见问题cell
+
+#import "ProblemTableViewCell.h"
+
+@implementation ProblemTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
