d7b2735b8d3afd7d03aa57f436e0c8de00676f5a
ACE <wangsunyu@yayuanzi.com>
2018-04-23 20:30:52 +0800

    UPDATE:我的收藏夹cell

--- pe762-ios/Classes/PersonCenter/Controller/MyRecordingViewController.m
+++ pe762-ios/Classes/PersonCenter/Controller/MyRecordingViewController.m
@@ -10,7 +10,14 @@
 
 @interface MyRecordingViewController ()
 {
+    UIButton *setingBtn; //设置按钮
+    UIButton *messageBtn; //消息按钮
     
+    //1 我的收藏 2 播放记录 3 我的下载
+    NSInteger type;
+    
+    UITableView *listTableView;
+    NSArray *listDataArray;
 }
 @end
 
@@ -19,6 +26,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self initNav];
+    [self initUI];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self showTabBarView:NO];
+}
+
+- (void)initNav {
+    self.view.backgroundColor = RGB(243, 243, 243);
+    [self createNavigationTitle:@"用户中心"];
+    
+    UIImageView *setImageView = [[UIImageView alloc] initWithFrame:CGRectMake(235 * kScreenWidthProportion, kStatusHeight + 12, 20, 20)];
+    setImageView.image = [UIImage imageNamed:@"Layer_-2"];
+    [self.view addSubview:setImageView];
+    
+    setingBtn = [[UIButton alloc] initWithFrame:CGRectMake(230 * kScreenWidthProportion, kStatusHeight, 30, 44)];
+    [setingBtn addTarget:self action:@selector(setingBtnAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:setingBtn];
+    
+    UIImageView *messageImageView = [[UIImageView alloc] initWithFrame:CGRectMake(275 * kScreenWidthProportion, kStatusHeight + 12, 18, 20)];
+    messageImageView.image = [UIImage imageNamed:@"Layer_1_1_"];
+    [self.view addSubview:messageImageView];
+    
+    messageBtn = [[UIButton alloc] initWithFrame:CGRectMake(270 * kScreenWidthProportion, kStatusHeight, 30, 44)];
+    [messageBtn addTarget:self action:@selector(messageBtnAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:messageBtn];
+    
+    [self createEndBackView];
+    
+}
+
+- (void)initUI {
+    
+}
+
+#pragma makr - 设置点击
+- (void)setingBtnAction {
+    [self.navigationController pushViewController:[SettingViewController new] animated:YES];
+}
+
+#pragma makr - 消息点击
+- (void)messageBtnAction {
+    
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ pe762-ios/Classes/PersonCenter/View/CollectionTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  CollectionTableViewCell.m
+//  pe762-ios
+//
+//  Created by wsy on 2018/4/23.
+//  Copyright © 2018年 zmit. All rights reserved.
+//  我的收藏夹cell
+
+#import "CollectionTableViewCell.h"
+
+@implementation CollectionTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
