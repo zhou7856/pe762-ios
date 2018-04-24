20bfa3dd4dea6e5e0637653cbfa6469b170de211
Future <liyongkui@yayuanzi.com>
2018-04-24 09:46:26 +0800

    ADDED:添加注册页面

---
+++ pe762-ios/Classes/Base/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  pe762-ios
+//
+//  Created by Future on 2018/4/24.
+//  Copyright © 2018年 zmit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
