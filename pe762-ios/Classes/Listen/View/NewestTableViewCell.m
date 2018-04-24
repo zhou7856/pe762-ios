34e5f7574246a7b8e984b03278e6478bb3cb75cb
Future <liyongkui@yayuanzi.com>
2018-04-24 18:54:16 +0800

    ADDED:添加上新cell

---
+++ pe762-ios/Classes/Listen/View/NewestTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  NewestTableViewCell.m
+//  pe762-ios
+//
+//  Created by Future on 2018/4/24.
+//  Copyright © 2018年 zmit. All rights reserved.
+//  上新cell
+
+#import "NewestTableViewCell.h"
+
+@implementation NewestTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
