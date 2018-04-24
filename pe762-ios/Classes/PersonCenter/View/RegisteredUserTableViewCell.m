f93a7702c74730a15d722df016ff34ed8a18c3f8
ACE <wangsunyu@yayuanzi.com>
2018-04-24 11:55:54 +0800

    UPDATE:川江注册用户cell

---
+++ pe762-ios/Classes/PersonCenter/View/RegisteredUserTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  RegisteredUserTableViewCell.m
+//  pe762-ios
+//
+//  Created by wsy on 2018/4/24.
+//  Copyright © 2018年 zmit. All rights reserved.
+//  注册用户cell
+
+#import "RegisteredUserTableViewCell.h"
+
+@implementation RegisteredUserTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
