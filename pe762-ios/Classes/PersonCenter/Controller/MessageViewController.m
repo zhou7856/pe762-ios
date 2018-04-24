16e3a2f627363a1aef8f5be4c31775e1f40870eb
Future <liyongkui@yayuanzi.com>
2018-04-24 10:42:10 +0800

    ADDED:添加消息页面文件

---
+++ pe762-ios/Classes/PersonCenter/Controller/MessageViewController.m
@@ -0,0 +1,37 @@
+//
+//  MessageViewController.m
+//  pe762-ios
+//
+//  Created by Future on 2018/4/24.
+//  Copyright © 2018年 zmit. All rights reserved.
+//  消息页面
+
+#import "MessageViewController.h"
+
+@interface MessageViewController ()
+
+@end
+
+@implementation MessageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
