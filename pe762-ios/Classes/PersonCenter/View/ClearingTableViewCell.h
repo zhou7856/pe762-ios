cd423880ef5ee6f0fc920c3601904e96b7aff06d
ACE <wangsunyu@yayuanzi.com>
2018-04-24 14:58:23 +0800

    ADDED:添加结算cell

---
+++ pe762-ios/Classes/PersonCenter/View/ClearingTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ClearingTableViewCell.h
+//  pe762-ios
+//
+//  Created by wsy on 2018/4/24.
+//  Copyright © 2018年 zmit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClearingTableViewCell : UITableViewCell
+
+@end
---
+++ pe762-ios/Classes/PersonCenter/View/ClearingTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ClearingTableViewCell.m
+//  pe762-ios
+//
+//  Created by wsy on 2018/4/24.
+//  Copyright © 2018年 zmit. All rights reserved.
+//
+
+#import "ClearingTableViewCell.h"
+
+@implementation ClearingTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
